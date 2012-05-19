9fd4a81b550511499aa5b8d54c2b58e8df2c4ce3
Xijin Zhang <mfs6174@gmail.com>
2012-05-20 00:52:39 +0800

    complete the readme

--- cliofetion.c
+++ cliofetion.c
@@ -1,8 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2012 by mfs6174                                         *
  *   mfs6174@gmail.com                                                     *
- *   Copyright (C) 2010 by lwp                                             *
- *   levin108@gmail.com                                                    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
